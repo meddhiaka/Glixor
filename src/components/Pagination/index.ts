06fda31abaa57023a20ab77fef33f6af787b373c
Med Dhia Kassab <108496649+meddhiaka@users.noreply.github.com>
2026-08-16 18:32:58 +0100

    export component and prop types

---
+++ src/components/Pagination/index.ts
@@ -0,0 +1,7 @@
+export { Pagination, PaginationStepper, PaginationJumpTo, PaginationLoadMore } from './Pagination'
+export type {
+  PaginationProps,
+  PaginationStepperProps,
+  PaginationJumpToProps,
+  PaginationLoadMoreProps,
+} from './Pagination'
