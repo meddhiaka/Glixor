f17e9305672dbd60f036aeb2d44c40887d9263ad
Med Dhia Kassab <108496649+meddhiaka@users.noreply.github.com>
2026-08-16 17:59:09 +0100

    add Card component with sub-components and types

---
+++ src/components/Card/index.ts
@@ -0,0 +1,14 @@
+export { Card, CardMedia, CardTitle, CardDescription, CardFooter } from './Card'
+export type {
+  CardProps,
+  CardSurface,
+  CardPadding,
+  CardRounding,
+  CardMediaProps,
+  CardMediaSize,
+  CardMediaShape,
+  CardTitleProps,
+  CardTitleSize,
+  CardDescriptionProps,
+  CardFooterProps,
+} from './Card'
