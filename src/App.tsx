2739168e664c15deca03fd5dcd333b7ca2cb18a9
meddhiakassab <dhia.kassab@speedykom.de>
2026-08-16 16:29:59 +0100

    add initial App component with homepage styling and Storybook story

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="home">
+      <h1>Glixor</h1>
+      <p>Your app starts here.</p>
+    </main>
+  )
+}
+
+export default App
